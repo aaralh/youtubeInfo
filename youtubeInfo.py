93b45d3e8db9317c273d1893e80f466b73ad5a2b
Aaro <aaro.alhainen@gmail.com>
2018-10-14 19:42:28 +0300

    Removed timing

--- youtubeInfo.py
+++ youtubeInfo.py
@@ -5,17 +5,6 @@ import logging
 import time
 from bs4 import BeautifulSoup as bs
 
-def timing(f):
-    def wrap(*args):
-        time1 = time.time()
-        ret = f(*args)
-        time2 = time.time()
-        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
-
-        return ret
-    return wrap
-
-@timing
 def __fetchVideoInfo(url):
     """ Fetch video info from url and return json.
 
